62be8d3040d4a37a5b489cb3361418efb3e5add6
AbdelrhmanEng <ght7234@gmail.com>
2025-07-15 02:11:14 +0300

    update

---
+++ Number Theory/FixMod.cpp
@@ -0,0 +1,20 @@
+//-----------------------------------------بسم الله الرحمن الرحيم-------------------------------------
+//----------------------------------اللهم صلي وسلم وبارك على نبينا محمد-------------------------------
+#include <bits/stdc++.h>
+#define ll long long
+#define F first
+#define S second
+#define ld long double
+#define Mbappe ios::sync_with_stdio(false);cin.tie(nullptr)
+using namespace std;
+ll fixMod(ll a , ll b) {
+    return (a % b + b)%b;
+}
+int main() {
+    Mbappe;
+    cout<<fixMod(-6,5);
+
+
+    return 0;
+}
+
