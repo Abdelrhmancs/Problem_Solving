9d63c490eb779af28e675a1786a4f1decfa5b0d9
Abdelrhmancs <ght7234@gmail.com>
2026-08-09 19:14:38 +0300

    Add trigonometric functions implementation in C++

---
+++ Graph/Geometry/sin_cos_tan.cpp
@@ -0,0 +1,45 @@
+// -----------------------------------------بسم الله الرحمن الرحيم-------------------------------------
+// ----------------------------------اللهم صلي وسلم وبارك على نبينا محمد-------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define F first
+#define S second
+#define all(v) v.begin(), v.end()
+#define Abdelrhman ios::sync_with_stdio(false); cin.tie(nullptr);
+#define MAX(v) *max_element(all(v))
+#define MIN(v) *min_element(all(v))
+
+double sind(double deg) {
+    return sin(deg * acos(-1) / 180.0);
+}
+
+double cosd(double deg) {
+    return cos(deg * acos(-1) / 180.0);
+}
+
+double tand(double deg) {
+    return tan(deg * acos(-1) / 180.0);
+}
+
+double asind(double x) {
+    return asin(x) * 180.0 / acos(-1);
+}
+
+double acosd(double x) {
+    return acos(x) * 180.0 / acos(-1);
+}
+
+double atand(double x) {
+    return atan(x) * 180.0 / acos(-1);
+}
+
+int main() {
+    Abdelrhman;
+
+    
+
+    return 0;
+}
